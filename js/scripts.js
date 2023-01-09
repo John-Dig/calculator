4108eefefa953cc16e1ea3483fb4c199bddd0ba0
John Diggins <rohnfiggins@gmail.com>
2023-01-09 11:15:04 -0800

    add, subtract, mutiply and divide done 2 different ways for reference

--- js/scripts.js
+++ js/scripts.js
@@ -12,14 +12,25 @@ saySomehting(`The sum is ${add(3,5)}.`);
 
 //business logic
 function add(number1, number2) {
-    return number1 + number2;
+    return `${number1} plus ${number2} is ${number1 + number2}`;
 }
+function subtract(number1, number2){
+    return number1 + " minus " + number2 + " equals " + (number1 - number2);
+}
+function multiply(number1, number2) {
+    return number1 + " times " + number2 + " is " + (number1 * number2);
+}
+function divide(number1, number2){
+    return `${number1} divided by ${number2} is ${number1 / number2}`;
+}
+
 
 //user interface logic
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
 
-window.alert(add(number1, number2));
+window.alert(divide(number1, number2));
+
 
 
